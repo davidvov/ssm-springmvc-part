2cd484681bec1e4bc2ea49578398e308f8a3ac5c
Code-BoyLT <1183816981@qq.com>
2025-12-11 17:15:50 +0800

    数据接收补充

---
+++ springmvc-base-input-02/src/main/java/com/atguigu/api/ApiController.java
@@ -0,0 +1,4 @@
+package com.atguigu.api;
+
+public class ApiController {
+}
---
+++ springmvc-base-input-02/src/main/java/com/atguigu/share/ShareController.java
@@ -0,0 +1,8 @@
+package com.atguigu.share;
+
+/**
+ * @author Mercurio
+ * @date 2025/12/11 16:08
+ * @description 演示共享域对象获取
+ */public class ShareController {
+}
